9f80875aca836766981067c05b24219978b8ea93
Christoph Cernusca <cernusca.c22@htlwienwest.at>
2024-12-10 23:12:16 +0100

    Networking Rework - Unfinished - Only 2 players - instant game start on accept - very limited control

--- main.py
+++ main.py
@@ -1,21 +1,96 @@
 
+import pygame as pg
 import scripts.networking as net
+import scripts.tetris as tetris
+import scripts.display as display
+
+def start():
+
+    net.start_invitation_listener()
+
+    while True:
+        query = input("> ")
+        if query == "":
+            print(net.current_invitation)
+            continue
+        match(query.split()[0]):
+            case "exit":
+                net.stop_listeners()
+                exit()
+
+            case "clear":
+                print("\033[H\033[J", end="")
+
+            case "ip":
+                print(net.get_own_ip())
+
+            case "invite":
+                ip = query.split()[1]
+                net.send_invitation(ip)
+
+            case "invitation":
+                net.get_current_invitation()
+
+            case "accept":
+                valid = net.current_invitation
+
+                net.accept_invitation()
+
+                # Start game
+
+                if valid:
+                    pg.init()
+                    pg.display.set_caption("Tetris")
+                    screen = pg.display.set_mode((800, 600))
+                    clock = pg.time.Clock()
+                    dt = 0
+
+                    game_display = display.GameDisplay(2)
+                    game_display.init_sprites()
+                    game = tetris.TetrisGame(game_display)
+
+                    while True:
+                        for event in pg.event.get():
+                            if event.type == pg.QUIT:
+                                pg.quit()
+                                exit()
+                            if event.type == pg.KEYDOWN:
+                                if event.key == pg.K_ESCAPE:
+                                    pg.quit()
+                                    exit()
+                                if event.key == pg.K_LEFT:
+                                    if game.current_piece_id is not None:
+                                        game.move_piece(game.current_piece_id, (0, -1))
+                                if event.key == pg.K_RIGHT:
+                                    if game.current_piece_id is not None:
+                                        game.move_piece(game.current_piece_id, (0, 1))
+                                if event.key == pg.K_DOWN:
+                                    if game.current_piece_id is not None:
+                                        game.hard_drop = True
+                                if event.key == pg.K_UP:
+                                    if game.current_piece_id is not None:
+                                        game.rotate_piece(game.current_piece_id, -1)
+                                if event.key == pg.K_RETURN:
+                                    game.start()
+                            if event.type == pg.KEYUP:
+                                if event.key == pg.K_DOWN:
+                                    game.hard_drop = False
+
+                        game.update(dt)
+
+                        net.send_update(game.board)
+                        game_display.display_game(0, game.board)
+                        if net.opponent_board is not None:
+                            game_display.display_game(1, net.opponent_board)
+                        game_display.update_screen(screen)
+
+                        dt = clock.tick(60) / 1000
+
+            case "decline":
+                net.decline_invitation()
+
+            case _:
+                print("Unknown command")
 
 if __name__ == "__main__":
-
-	print(f"Your IP-Address: {net.get_own_ip()}")
-
-	listener = net.start_invitation_listener()
-	manager = net.start_manager()
-
-	net.create_party()
-
-	while net.running:
-		
-		# Do actions
-		net.query_action()
-
-	listener.join()
-	manager.join()
-
-	print("Program stopped")
+    start()
--- scripts/networking.py
+++ scripts/networking.py
@@ -2,70 +2,48 @@
 import socket
 import threading
 import time
-import json
 import pygame as pg
 import numpy as np
 import scripts.tetris as tetris
 import scripts.display as display
 
-PORT = 33333
+INVITATION_PORT = 33333
+INVITATION_RESPONSE_PORT = 33334
+UPDATE_PORT = 33335
 
-current_invitation = {}
-open_invitations = {}
+current_invitation = None
+current_opponent = None
 
-host = None
-players = {}
+opponent_board = None
 
-to_kick = []
-to_join = []
-
-running = True
-stop_listener = threading.Event()
-stop_manager = threading.Event()
+stop_invitation_listener = threading.Event()
+stop_receive_listener = threading.Event()
+stop_update_listener = threading.Event()
 
 def get_own_ip():
 	with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
 		s.connect(("8.8.8.8", 80))
 		return s.getsockname()[0]
 
-def establish_connection(ip, port=PORT):
-	print(f"Trying to connect to {ip}:{port}...")
-	try:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect((ip, port))
-		s.settimeout(0.1)
-		print(f"Successfully connected to {ip}:{port}")
-		return s
-	except socket.gaierror:
-		print(f"Invalid IP address: {ip}")
-	except ConnectionRefusedError:
-		print(f"Connection refused by {ip}:{port}")
-	except TimeoutError:
-		print(f"Connection to {ip}:{port} timed out")
-	except Exception as e:
-		print(f"An unexpected error occurred: {e}")
-	return None
-
 def start_invitation_listener():
 	def invite_listener():
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.bind(("0.0.0.0", PORT))
+		s.bind(("0.0.0.0", INVITATION_PORT))
 		s.listen(1)
 		s.settimeout(0.1)
-		print(f"Listening for invitations on port {PORT}")
-		while not stop_listener.is_set():
+		print(f"Started invitation listener at {get_own_ip()}:{INVITATION_PORT}")
+		while not stop_invitation_listener.is_set():
 			try:
 				conn, addr = s.accept()
 				if conn.recv(1024).decode() == "INVITE":
 					ip = addr[0]
-					print(f"\rReceived invitation from {ip}\n> ", end="")
+					print(f"Received invitation from {ip}")
 					global current_invitation
-					current_invitation = {ip: conn}
+					current_invitation = ip
 			except socket.timeout:
 				continue
 		s.close()
-		print("Invitation listener stopped")
-	
+
 	listener = threading.Thread(target=invite_listener)
 	listener.start()
 
@@ -73,388 +51,118 @@ def start_invitation_listener():
 
 	return listener
 
-def answer_invitations():
-	global current_invitation
-	if current_invitation:
-		ip, sock = current_invitation.popitem()
-		if input(f"Accept invitation from {ip}? (y/n): ").lower() == "y":
-			sock.send("JOIN".encode())
-			global host, players
-
-			if host is None:
-				# Dissolve party
-				dissolve_party()
-			else:
-				# Leave party
-				print(f"Leaving party of {host['ip']}")
-				send_host("LEAVE")
-
-			# Join party of ip
-			host = {"ip": ip, "socket": sock}
-			players = {}
-		else:
-			sock.send("DECLINE".encode())
-			sock.close()
-		current_invitation = ""
-
-def send_all(message: str):
-	if host is None:
-		for player in players:
-			send_player(player, message)
-
-def send_host(message: str):
-	if host is not None:
-		host["socket"].send(message.encode())
-
-def send_player(player, message: str):
-	if player not in players:
-		print(f"{player} is not in the party")
-	else:
-		players[player].send(message.encode())
-
-def invite_player(ip):
-	sock = establish_connection(ip)
-	if sock is not None:
-		sock.send("INVITE".encode())
-		print(f"Invited {ip}")
-	else:
-		print(f"Failed to invite {ip}")
-	open_invitations[ip] = sock
-
-def kick_player(ip):
-	send_player(ip, "KICK")
-	players[ip].close()
-	global to_kick
-	to_kick.append(ip)
-	print(f"Kicked {ip}")
-
-def create_party():
-	global host, players
-	host = None
-	players = {}
-	print("Party created")
-
-def dissolve_party():
-	global host, players
-	send_all("DISSOLVE")
-	for player in players:
-		players[player].close()
-	players = {}
-	print("Party dissolved")
-
-def query_action():
-
-	global host, players, to_kick, to_join
-	
-	for ip in to_kick:
-		if ip in players:
-			players.pop(ip)
-	to_kick = []
-	for ip, sock in to_join:
-		players[ip] = sock
-	to_join = []
-	action = input("> ").split()
-
-	# Ignore empty lines
-	if len(action) == 0:
-		return
-
-	# Invite ip to party, if you are host
-	elif action[0] == "invite":
-		if host is None:
+def start_invitation_response_listener():
+	def invitation_response_listener():
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.bind(("0.0.0.0", INVITATION_RESPONSE_PORT))
+		s.listen(1)
+		s.settimeout(0.1)
+		print(f"Started invitation response listener at {get_own_ip()}:{INVITATION_RESPONSE_PORT}")
+		while not stop_receive_listener.is_set():
 			try:
-				if action[1] == get_own_ip():
-					print("You cannot invite yourself")
-				else:
-					invite_player(action[1])
-			except IndexError:
-				print("Missing IP address")
-		else:
-			print(f"Only the host ({host["ip"]}) can invite players")
-	
-	# Answer invitations
-	elif action[0] == "invitation":
-		if current_invitation:
-			answer_invitations()
-		else:
-			print("No invitations")
+				conn, addr = s.accept()
+				if conn.recv(1024).decode() == "ACCEPT":
+					print(f"{addr[0]} accepted invitation")
+					conn.close()
+					break
+				elif conn.recv(1024).decode() == "DECLINE":
+					print(f"{addr[0]} declined invitation")
+					conn.close()
+					break
+			except socket.timeout:
+				continue
+		s.close()
 
-	# Leave party / stop program if you are host
-	elif action[0] == "leave":
-		if host is None:
-			stop_all()
-		else:
-			print("\rLeaving party")
-			send_host("LEAVE")
-			create_party()
-	
-	# Kick player from party
-	elif action[0] == "kick":
-		if host is None:
-			if action[1] in players:
-				try:
-					if action[1] == get_own_ip():
-						print("You cannot kick yourself")
-					else:
-						kick_player(action[1])
-				except IndexError:
-					print("Missing IP address")
-			else:
-				print(f"{action[1]} is not in the party")
-		else:
-			print(f"Only the host ({host['ip']}) can kick players")
-	
-	# List players
-	elif action[0] == "list":
-		if host is None:
-			print(f"Host: You ({get_own_ip()})")
-			if len(players) == 0:
-				print("No players")
-			else:
-				[print(player) for player in players]
-		else:
-			send_host("LIST")
-			while len(players) == 0:
-				time.sleep(0.1)
-			print(f"\rHost: {host['ip']}\n" + "\n".join([player for player in players]))
-	
-	# Start tetris game
-	elif action[0] == "start":
-		if host is None:
-			send_all("START")
-			start_tetris_game()
-		else:
-			print("Only the host can start the game")
-	
-	else:
-		print("Unknown command")
-
-def start_tetris_game():
-
-	UPDATE_FREQUENCY_CHANGE_FACTOR = 1.5
-
-	game = tetris.TetrisGame()
-	game.update_frequency = 5
-
-	if host is not None:
-		send_host("LIST")
-		while len(players) == 0:
-			time.sleep(0.1)
-
-	global tetris_players, games_display
-	tetris_players = {ip: i + 1 for i, ip in enumerate(players.keys())}
-	tetris_players[get_own_ip() if host is None else host["ip"]] = 0
-
-	games_display = display.GameDisplay(len(tetris_players), tetris.BOARD_SIZE)
-	games_display.init_sprites()
-
-	pg.init()
-	pg.display.set_caption('Tetris')
-	screen = pg.display.set_mode((1280, 720), pg.RESIZABLE)
-	clock = pg.time.Clock()
-	dt = 0
-
-	running = True
-
-	while running:
-
-		for event in pg.event.get():
-			if event.type == pg.QUIT:
-				running = False
-			if event.type == pg.KEYDOWN:
-				if event.key == pg.K_ESCAPE:
-					running = False
-				if event.key == pg.K_o:
-					game.debug = not game.debug
-					games_display.debug = not games_display.debug
-				if event.key == pg.K_LEFT:
-					if game.current_piece_id is not None:
-						game.move_piece(game.current_piece_id, (0, -1))
-				if event.key == pg.K_RIGHT:
-					if game.current_piece_id is not None:
-						game.move_piece(game.current_piece_id, (0, 1))
-				if event.key == pg.K_DOWN:
-					game.hard_drop = True
-				if event.key == pg.K_UP:
-					if game.current_piece_id is not None:
-						game.rotate_piece(game.current_piece_id, -1)
-				if event.key == pg.K_RETURN:
-					game.start()
-				if event.key == pg.K_HASH:
-					game.resume() if game.stopped else game.stop()
-				if event.key == pg.K_PLUS:
-					game.change_update_frequency(UPDATE_FREQUENCY_CHANGE_FACTOR)
-				if event.key == pg.K_MINUS:
-					game.change_update_frequency(1 / UPDATE_FREQUENCY_CHANGE_FACTOR)
-			if event.type == pg.KEYUP:
-				if event.key == pg.K_DOWN:
-					game.hard_drop = False
-
-		game.update(dt)
-
-		send_game_state(game.board, game.pieces)
-		
-		games_display.display_game(0, game.board, game.pieces)
-		games_display.update_screen(screen)
-
-		dt = clock.tick(60) / 1000
-	
-	pg.quit()
-
-def send_game_state(game_board: np.ndarray, game_pieces: dict):
-	game_board = list(game_board.tolist())
-	game_pieces = {k: {"color": v["color"], "position": list(v["position"].tolist()), "piece": list(v["piece"].tolist())} for k, v in game_pieces.items()}
-	if host is None:
-		send_all("STATE" + json.dumps({"player": get_own_ip(), "board": game_board, "pieces": game_pieces}))
-	else:
-		send_all("STATE" + json.dumps({"player": get_own_ip(), "board": game_board, "pieces": game_pieces}))
-
-def start_manager():
-	global stop_manager
-	stop_manager.clear()
-	def manage():
-		global host, players
-		while not stop_manager.is_set():
-			# Host jobs
-			if host is None and players is not None:
-				global to_kick, to_join
-				try:
-					for player in players:
-						sock = players[player]
-						try:
-							request = sock.recv(1024)
-
-							if request.decode() == "LEAVE":
-								sock.close()
-								to_kick.append(player)
-								print(f"\r{player} left party\n> ", end="")
-							
-							elif request.decode() == "LIST":
-								info = {"host": get_own_ip(), "players": list(players.keys())}
-								sock.send(("LIST" + json.dumps(info)).encode())
-							
-							elif request.decode().startswith("STATE"):
-								info = json.loads(request.decode().strip("STATE"))
-								send_game_state("STATE" + json.dumps(info))
-								print(info)
-								games_display.display_game(tetris_players[info["player"]], info["board"], info["pieces"])
-							
-							else:
-								print(f"\rUnknown request {request.decode()} from {player}\n> ", end="")
-						
-						except socket.timeout:
-							pass
-
-						except OSError:
-							sock.close()
-							to_kick.append(player)
-				
-				except RuntimeError:
-					pass
-					
-				for ip in to_kick:
-					if ip in players:
-						players.pop(ip)
-				to_kick = []
-				for ip, sock in to_join:
-					players[ip] = sock
-				to_join = []
-				
-				global open_invitations
-				for invitation in open_invitations:
-					sock = open_invitations[invitation]
-					if sock is not None:
-						try:
-							request = sock.recv(1024)
-
-							if request.decode() == "DECLINE":
-								print(f"\r{invitation} declined invitation\n> ", end="")
-								sock.close()
-								open_invitations[invitation] = None
-							
-							elif request.decode() == "JOIN":
-								print(f"\r{invitation} joined party\n> ", end="")
-								players[invitation] = sock
-								
-						except socket.timeout:
-							pass
-
-						except OSError:
-							sock.close()
-							to_kick.append(player)
-				open_invitations = {invitation: open_invitations[invitation] for invitation in open_invitations if open_invitations[invitation] is not None}
-			
-			# Player jobs
-			elif host is not None:
-				try:
-					request = host["socket"].recv(1024)
-
-					def create_party():
-						global host, players
-						host = None
-						players = {}
-						print("Party created\n> ", end="")
-
-					if request.decode() == "DISSOLVE":
-						print("\rParty dissolved by host")
-						create_party()
-					
-					elif request.decode() == "KICK":
-						print("\rYou were kicked out")
-						create_party()
-							
-					elif request.decode().startswith("LIST"):
-						info = json.loads(request.decode().strip("LIST"))
-						players = {ip: None for ip in info["players"]}
-					
-					elif request.decode() == "START":
-						print("\rGame started\n> ", end="")
-						threading.Thread(target=start_tetris_game).start()
-							
-					elif request.decode().startswith("STATE"):
-						info = json.loads(request.decode().strip("STATE"))
-						print(info)
-						games_display.display_game(tetris_players[info["player"]], np.array(info["board"]), {info["pieces"][k]: {"color": v["color"], "position": np.array(v["position"]), "piece": np.array(v["piece"])} for k, v in info["pieces"].items()})
-					
-					else:
-						print(f"\rUnknown request {request.decode()} from host\n> ", end="")
-					
-				except socket.timeout:
-					pass
-		
-		# Manager stopped, which means player is exiting program
-		dissolve_party()
-		send_host("LEAVE")
-	
-	manager = threading.Thread(target=manage)
-	manager.start()
+	listener = threading.Thread(target=invitation_response_listener)
+	listener.start()
 
 	time.sleep(0.1)  # Allow the thread to start before continuing
 
-	return manager
+	return listener
 
-def stop_all():
-	print("Stopping program")
-	global stop_listener, stop_manager, running
-	stop_manager.set()
-	stop_listener.set()
-	running = False
+def start_update_listener():
+	def update_listener():
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.bind(("0.0.0.0", UPDATE_PORT))
+		s.listen(1)
+		s.settimeout(0.1)
+		print(f"Started update listener at {get_own_ip()}:{UPDATE_PORT}")
+		while not stop_update_listener.is_set():
+			try:
+				conn, addr = s.accept()
+				if conn.recv(1024).decode().startswith("UPDATE"):
+					print(f"Received update from {addr[0]}")
+					global opponent_board
+					opponent_board = decode_board(conn.recv(1024).decode().strip("UPDATE"))
+			except socket.timeout:
+				continue
+		s.close()
 
-if __name__ == "__main__":
+	listener = threading.Thread(target=update_listener)
+	listener.start()
 
-	print(f"Your IP-Address: {get_own_ip()}")
+	time.sleep(0.1)  # Allow the thread to start before continuing
 
-	listener = start_invitation_listener()
-	manager = start_manager()
+	return listener
 
-	create_party()
+def get_current_invitation():
+	global current_invitation
+	if current_invitation:
+		print(f"Invitation from {current_invitation}")
+	else:
+		print("No invitation")
 
-	while running:
-		
-		# Do actions
-		query_action()
+def send_invitation(ip):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((ip, INVITATION_PORT))
+	s.send("INVITE".encode())
+	s.close()
+	print(f"Invitation sent to {ip}")
+	start_invitation_response_listener()
 
-	listener.join()
-	manager.join()
+def accept_invitation():
+	global current_invitation, current_opponent
+	if current_invitation:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((current_invitation, INVITATION_RESPONSE_PORT))
+		s.send("ACCEPT".encode())
+		s.close()
+		print(f"Invitation from {current_invitation} accepted")
+		current_opponent = current_invitation
+		current_invitation = None
+		start_update_listener()
+	else:
+		print("No invitation")
 
-	print("Program stopped")
+def decline_invitation():
+	global current_invitation
+	if current_invitation:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((current_invitation, INVITATION_RESPONSE_PORT))
+		s.send("DECLINE".encode())
+		s.close()
+		print(f"Invitation from {current_invitation} declined")
+		current_invitation = None
+	else:
+		print("No invitation")
+
+def stop_listeners():
+	stop_invitation_listener.set()
+	stop_receive_listener.set()
+	stop_update_listener.set()
+
+def encode_board(board):
+	return "".join(str(x) for x in np.array(board).flatten())
+
+def decode_board(board):
+	return np.array([int(x) for x in board]).reshape(*tetris.BOARD_SIZE)
+
+def send_update(board):
+	global current_opponent
+	if current_opponent:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((current_opponent, UPDATE_PORT))
+		s.send(f"UPDATE{encode_board(board)}".encode())
+		s.close()
+	else:
+		print("No opponent")
